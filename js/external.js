ab13b66b491aed9732cc07e046a0c675af3e2e40
Steven Lacy <steven.lacy11@gmail.com>
2021-08-12 14:43:09 -0500

    lecture changes

---
+++ js/external.js
@@ -0,0 +1,13 @@
+"use strict";
+
+    console.log("Hello from External js");
+    //var myStr = 45;
+   // alert("This is definitely the external file");
+    //var userConfirm = confirm("Do you want to remained logged in?");
+   // alert("The user selected: " + userConfirm)
+    // You can use let in place of var.
+    var userLunch = prompt("What did you have for lunch?");
+
+    alert("The user had " + userLunch + " for lunch!");
+
+

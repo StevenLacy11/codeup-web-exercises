c943bf350831508a0f482347c09fdc2fd888666f
Steven Lacy <steven.lacy11@gmail.com>
2021-08-17 16:04:01 -0500

    Feat: Loops practice

---
+++ js/for_loops.js
@@ -0,0 +1,13 @@
+
+// var number = parseInt(prompt("enter a whole number"));
+//
+// for(var i = 1; i <= 10; i++) {
+//     var result = i * number;
+//     console.log(result);
+// }
+
+
+for (var i = "1"; i.length < 10; i++) {
+    var result =  i.length ++
+    console.log(i);
+}

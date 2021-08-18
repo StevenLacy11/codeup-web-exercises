9b4d9d8b49abd537c361ce791d8c52f3e731bc3d
Steven Lacy <steven.lacy11@gmail.com>
2021-08-18 11:34:25 -0500

    Feat: Loops exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,4 +1,4 @@
-
+                        //2
 // var number = parseInt(prompt("enter a whole number"));
 //
 // for(var i = 1; i <= 10; i++) {
@@ -6,8 +6,34 @@
 //     console.log(result);
 // }
 
+//                        //3
+// function randomNum(min, max) {
+//     for (var x = 10; x <= 200; x++) {
+//         if (x === 0) {
+//             console.log(x + " is even");
+//         } else if (x % 2 === 0) {
+//             console.log(x + " is even");
+//         } else {
+//             console.log(x + " is odd");
+//         }
+//     }
+//     return parseInt(Math.random() * (max - min) + min);
+// }
 
-for (var i = "1"; i.length < 10; i++) {
-    var result =  i.length ++
-    console.log(i);
-}
+
+                    //4
+// function oneOne() {
+//
+//     for (var i = 1; i <= 9; i++) {
+//         console.log(i.toString().repeat(i));
+//     }
+// }
+
+
+                    //5
+ // function countDown(){
+ //    for (var i = 100; i >= 5; i = i - 5)
+ //    console.log(i)
+ //
+ // }
+

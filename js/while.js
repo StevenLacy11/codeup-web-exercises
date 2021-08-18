7c4454b6f4ed863be864d2dcdfdf3c965ef1e13a
Steven Lacy <steven.lacy11@gmail.com>
2021-08-18 11:34:33 -0500

    Feat: Loops exercises

---
+++ js/while.js
@@ -0,0 +1,29 @@
+// function twoTimes() {
+//     var i = 2
+//
+//     while (i < 17) {
+//
+//         console.log(2 ** i);
+//         i++;
+//     }
+// }
+
+
+
+
+                    // do- While
+// This is how you get a random number between 50 and 100
+
+// This expression will generate a random number between 1 and 5
+Math.floor(Math.random() * 5) + 1;
+
+do {
+    var allCones = Math.floor(Math.random() * 50) + 50;
+}
+while (allCones <= 50) {
+
+    allCones++;
+}
+
+console.log(allCones);
+

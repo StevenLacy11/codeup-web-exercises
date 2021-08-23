b038617946cf76b85af5eded82f2932c67d68301
Steven Lacy <steven.lacy11@gmail.com>
2021-08-23 14:18:14 -0500

    Feat: complete object lecture.

--- js/object-lecture.js
+++ js/object-lecture.js
@@ -46,7 +46,7 @@
 //* TODO: Create a new Object and call it 'myCar' using either way to do so shown above (object instance or object literal)
 
 var myCar = {};
-
+var car2 = {};
 // console.log(myCar)
 
 // That's great! But our car object doesn't have much going on, does it? There's no depth or meat to it - how can we fill our object in?
@@ -68,6 +68,13 @@ myCar.make = "Subaru";
 
 myCar["model"] = "WRX";
 
+
+// var Car2 = {
+// 	make: "chrysler",
+// 	model: "pacifica",
+// 	color: "silver",
+// 	year: 2017
+// };
 //In general, dot notation is preferred of the two notations
 //We can also set up our objects and properties/methods during object creation
 
@@ -75,7 +82,7 @@ myCar["model"] = "WRX";
 // TODO: Fill in your myCar object with properties of make, model, and year with the correct values. If you don't own a vehicle, fill in the details with your favorite vehicle.
 
 //For me? I'm missing the year for myCar so. .
-// myCar.year = 2020;
+myCar.year = 2020;
 // console.log(myCar);
 
 //Now I have a completed example of the myCar object - the model and make were added above in prior examples
@@ -97,7 +104,7 @@ myCar["model"] = "WRX";
 
 var myCarYear = myCar.year;
 
-// alert("Woops, I forgot to tell y'all what year my vehicle was " + myCarYear);
+// alert("Whoops, I forgot to tell y'all what year my vehicle was " + myCarYear);
 
 //What if we wanted to create a more complicated piece of data? Perhaps a list of our cars FEATURES?
 
@@ -129,23 +136,52 @@ var carOwner = {
 
 myCar.owner = carOwner;
 
-console.log(myCar);
+// console.log(myCar);
 
 // TODO: Nest a 'carOwner' object of your own into this growing myCar object and console.log the results!
 
 //An object can also contain within itself a method (function). We can set it up similarly to our properties, but with a value that is the function expression
+//property:num
+//property: "string"
+//* property: function(){....} //   NOTE no function name
 
+var turnMyCarOn = function (){
+	alert("Putting my key in the ignition and fired up the " + this.make + " " + this.model);
+};
 
+//option A
+myCar.turnOn = turnMyCarOn;
+
+//option B
+// 	myCar.turnOn = function () {
+// 		alert("Putting my key in the ignition and fired up the " + this.make + " " + this.model);
+// };
 /*
 * * * * * * * * * * * * * * * * * * * * * * * * * *
 *                 Arrays of Objects               *
 * * * * * * * * * * * * * * * * * * * * * * * * * *
 */
-
+var Car2 = {
+	make: "chrysler",
+	model: "pacifica",
+	color: "silver",
+	year: 2017,
+	owner: {firstName: "Steven",
+		lastName: "Lacy"},
+		turnOn:turnMyCarOn
+};
 //Let's tie some things together: Let's make a garage with another car!
+
+var theGarage = [myCar, Car2];
+
+console.log(theGarage)
 
 //Now: loop through an ARRAY of OBJECTS, accessing our turnOn function for our vehicles
 //Also note: we used the keyword *this* on the other vehicle's turnOn function, so this will be a good experiment to see the results
+
+theGarage.forEach(function(car){
+	car.turnOn();
+})
 
 
 //One final TODO: A bigger task - set up your own garage and add at least another vehicle into it. Log your garage array to verify it contains the cars you want - once your garage has the right cars, write a loop to access some properties or a method from them!

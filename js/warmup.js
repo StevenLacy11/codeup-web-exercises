e8e09850ac71474d6f9d001bcc8de02f14b9f129
Steven Lacy <steven.lacy11@gmail.com>
2021-09-14 09:47:30 -0500

    tacoTuesday Warm up.

--- js/warmup.js
+++ js/warmup.js
@@ -1,20 +1,39 @@
-function calculateDogAge(age, conversion) {
-		var dogYears = age*conversion;
+// function calculateDogAge(age, conversion) {
+// 		var dogYears = age*conversion;
+//
+// 		console.log("Your dog is " + dogYears + " years old in dog years! " + "We used " + conversion + " to get your dogs age.");
+//
+// 	}
+// calculateDogAge(4, 7);
+// calculateDogAge(2, 7);
+// calculateDogAge(10, 7);
+//
+// function calculateHumanAge(age, conversion){
+// 	var humanYears = age/conversion;
+//
+// 	console.log("You are " + parseInt(humanYears) + " years old in dog years!" + " We used " + conversion + " to get your age in  dog years.");
+// }
+// calculateHumanAge(26, 7)
+// calculateHumanAge(24, 7)
+// calculateHumanAge(1, 7)
 
-		console.log("Your dog is " + dogYears + " years old in dog years! " + "We used " + conversion + " to get your dogs age.");
 
+var hasTacos = ["blueberry muffin", "banana", "yogurt", "cereal", "brisket and egg taco"]
+
+var noTacos = ["parfait", "omlette de frumage", "protein shake", "fasting"]
+
+tacoTuesdays(hasTacos)
+tacoTuesdays(noTacos)
+
+function tacoTuesdays(arr){
+
+	for (var i = 0; i < arr.length; i++) {
+		if(arr[i].includes("taco")){
+	return true;
+		}
 	}
-calculateDogAge(4, 7);
-calculateDogAge(2, 7);
-calculateDogAge(10, 7);
-
-function calculateHumanAge(age, conversion){
-	var humanYears = age/conversion;
-
-	console.log("You are " + parseInt(humanYears) + " years old in dog years!" + " We used " + conversion + " to get your age in  dog years.");
+	return false;
 }
-calculateHumanAge(26, 7)
-calculateHumanAge(24, 7)
-calculateHumanAge(1, 7)
+
 
 

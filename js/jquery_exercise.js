2cc4c1ce82e5976ad25ac03b49b58145159b5e18
Steven Lacy <steven.lacy11@gmail.com>
2021-09-28 14:02:30 -0500

    jQuery exercises complete

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -1,5 +1,25 @@
 "use strict";
 
-$(document).ready(function () {
-	alert("Document ready - Javascript is ready to rock and roll!")
-});
+//~~~~~~~~~~~~~Elements~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+$("*").css("font-family", "Indie Flower").css("background-color", "#00ffcc");
+
+$("h1").css("font-family", "'Oswald', sans-serif").css("color", "#FF0033").css("text-align", "center").css("font-size", "3em");
+
+$("li").css("background-color", "#FF0033").css("font-size", "20px").css("color", "#FF8099");
+
+
+//~~~~~~~~~~~~~IDs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+$("#first-p").css("background-color", "#FF8099").css("font-size", "20px").css("color", "#00ffcc");
+
+$("#second-p").css("background-color", "#FF8099").css("font-size", "20px").css("margin", "20px").css("padding", "20px").css("border", "7px solid #FF0033").css("color", "#00ffcc");
+
+//~~~~~~~~~~~~~~~~Classes~~~~~~~~~~~~~~~~~~~~~~~~~
+
+$(".fa-li").css("background-color", "#FF0033").css("font-size", "20px");
+
+$(".fas").css("background-color", "#FF0033");
+
+$(".container").css("background-color", "#FF8099").css("margin", "20px").css("padding", "20px").css("border", "7px solid #FF0033");
+

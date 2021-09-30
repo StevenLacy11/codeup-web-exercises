711f0645ac7a133fe8b401110a4e5ed6a4cc2139
Steven Lacy <steven.lacy11@gmail.com>
2021-09-30 16:23:37 -0500

    FEAT: completed faq!

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -12,3 +12,21 @@ $("dt").click(function () {
 	$(this).css({"background-color": "yellow", "width": "10%"})
 });
 
+$("#btn2").click(function () {
+	$("ul").each(function () {
+		$(this).children().last().css({"background-color": "yellow", "width": "10%"})
+	})
+});
+
+$("h3").click(function () {
+	$("ul").each(function () {
+		$(this).children().css("font-weight", "bold")
+	})
+});
+
+$("li").click(function () {
+	$("ul").each(function () {
+		$(this).children().first().css("color", "blue")
+	})
+});
+

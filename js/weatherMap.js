d66fa7d18238ea916a81af1c8a5648957508a212
Steven Lacy <steven.lacy11@gmail.com>
2021-10-08 15:16:50 -0500

    Marker WORKS!!!!!

--- js/weatherMap.js
+++ js/weatherMap.js
@@ -2,85 +2,114 @@
 
 $(document).ready(function () {
 
-	//~~~~~~ Grabs the weather~~~~~~~~~~~~~~
-	$.get("http://api.openweathermap.org/data/2.5/weather", {
+	var weatherOptions = {
 		APPID: weatherKey,
-		q: "New Braunfels, US",
 		units: "imperial",
-	})
-		.done(function (data) {
-			console.log(data)
+		lat: 29.6928,
+		lon: -98.1122
+	};
 
+
+	mapboxgl.accessToken = mapboxKey;
+	const map = new mapboxgl.Map({
+		container: 'map',
+		maxZoom: 12,
+		minZoom: 6,
+		zoom: 12,
+		center: [-75.789, 41.874],
+	});
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~map~~~~~~~~~~~~~~~~~~~~
+	map.setStyle("mapbox://styles/mapbox/satellite-streets-v11")
+	map.setZoom(7)
+	map.setCenter([-98.1152, 29.6970])
+
+	var marker = new mapboxgl.Marker({
+		color: "darkBlue",
+	}).setLngLat({lng: -98.1122, lat: 29.6928}).addTo(map).setDraggable(true)
+
+		marker.on("dragend", () => {
+		var results = marker.getLngLat();
+		console.log(results.lat)
+		weatherOptions.lat = results.lat
+		weatherOptions.lon = results.lng
+		console.log(results)
+		console.log(weatherOptions)
+
+		renderFiveDays()
+	})
+
+
+	// .addTo(map);
+
+	//~~~~~~ Grabs the weather~~~~~~~~~~~~~~
+	renderFiveDays();
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~These are the current variables~~~~~~~~~~~~~~~
+	function renderFiveDays() {
+		$.get("http://api.openweathermap.org/data/2.5/forecast", weatherOptions)
 
-			var current_name = data.name
-			var current_temp_max = data.main.temp_max
-			var current_temp_min = data.main.temp_min
-			var current_temp = data.main.temp
-			var current_feels_like = data.main.feels_like
-			var current_humidity = data.main.humidity
-			var current_img = '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png"  alt="weather image"/>'
+			.done(function(data) {
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Need to capitalize ~~~~~~~~~~~~~~~~~~~~
-
-			var current_sky_description = data.weather[0].description
-
-
-//~~~~~~~~~~~~~~~~~~~~~ Test each path of variables!!!!!!!!!!!!!!!!! ~~~~~~~~~~~~~~~~~~~~
-
-			console.log(current_temp_max)
-
-// ~~~~~~~~This puts the data to the HTML Current ~~~~~~~~~
-			$(".current-title").html(current_name)
-
-			$(".current-text").html("Currently " + Math.round(current_temp) + "°" + "F" + "</br>" + current_img + "</br> with " + current_sky_description + "</br>" + "Feels like " + Math.round(current_feels_like) + "°" + "F</br>" + "Day " + Math.round(current_temp_max) + "°" + "F" + " ↑</br>" + "Night " + Math.round(current_temp_min) + "°" + "F" + " ↓</br>" + "Humidity " + current_humidity + "%</br>")
-
-
-		})
-})
-
-$.get("http://api.openweathermap.org/data/2.5/forecast", {
-	APPID: weatherKey,
-	q: "New Braunfels, US",
-	units: "imperial"
-})
-
-	.done(function (data) {
-		console.log(data.list)
-
+				$(".container").html("")
 //~~~~~~~~~~~~~~~These are the five day variables~~~~~~~~~~~~
-		for (var i = 0; i < data.list.length; i += 8) {
-			var name = data.city.name
-			var temp_max = data.list[i].main.temp_max
-			var temp_min = data.list[i].main.temp_min
-			var temp = data.list[i].main.temp
-			var feels_like = data.list[i].main.feels_like
-			var humidity = data.list[i].main.humidity
-			var sky_description = data.list[i].weather[0].description
-			var date = new Date(data.list[i].dt * 1000).toLocaleDateString("en-US")
-			console.log(data.list[i])
-			var img = '<img src="http://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png"  alt="weather image"/>'
+				for (var i = 0; i < data.list.length; i += 8) {
+					var name = data.city.name
+					var temp_max = data.list[i].main.temp_max
+					var temp_min = data.list[i].main.temp_min
+					var temp = data.list[i].main.temp
+					var feels_like = data.list[i].main.feels_like
+					var humidity = data.list[i].main.humidity
+					var sky_description = data.list[i].weather[0].description
+					var date = new Date(data.list[i].dt * 1000).toLocaleDateString("en-US", {})
+					var img = '<img src="http://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png"  alt="weather image"/>'
 
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Need to capitalize ~~~~~~~~~~~~~~~~~~~~
+					console.log(data.list[0].dt)
 
 
 //~~~~~~~~~~~~~~~~~~~~~ Test each path of variables!!!!!!!!!!!!!!!!! ~~~~~~~~~~~~~~~~~~~~
 
-			console.log(data.list[i].weather[0].icon)
 
 // ~~~~~~~~~~~~~~~~~~~~~This puts the data to the HTML five day ~~~~~~~~~
 
-			var html = '<div class="card border text-center">'
-				+ '<h6 class="card-header">' + name + "<br>" + date + '</h6>' + ("Average " + Math.round(temp) + "°" + "F" + "</br>" + img + "</br> with " + sky_description + "</br>" + "Feels like " + Math.round(feels_like) + "°" + "F</br>" + "Day " + Math.round(temp_max) + "°" + "F" + " ↑</br>" + "Night " + Math.round(temp_min) + "°" + "F" + " ↓</br>" + "Humidity " + humidity + "%</br>")
-				+ '</div>' + "</br>"
+					var html = '<div class="card border text-center">'
+						+ '<h6 class="card-header">' + name + "<br>" + date + '</h6>'
+						+ ("Average " + Math.round(temp) + "°" + "F" + "</br>" + img + "</br> with " + sky_description + "</br>" + "Feels like " + Math.round(feels_like) + "°" + "F</br>" + "Day " + Math.round(temp_max) + "°" + "F" + " ↑</br>" + "Night " + Math.round(temp_min) + "°" + "F" + " ↓</br>" + "Humidity " + humidity + "%</br>")
+						+ '</div>' + "</br>"
+
+					$('.container').append(html);
+				}
+			})
+	}
 
 
-			$('.container').append(html);
+})
 
-		}
-	});
+
+//~~~~~~~~~~~~~~~~~~~~~~marker~~~~~~~~~~~~~~~~~~~~~~~
+
+
+//~~~~~~~~~~~~~~~~~geocode~~~~~~~~~~~~~~~~~~~~~
+
+// function dropPin(data) {
+// 	geocode(data.location, mapboxKey).then(function (location) {
+// 		var popup = new mapboxgl.Popup()
+// 		var Marker = new mapboxgl.Marker()
+// 			.setLngLat(location)
+// 			.addTo(map)
+// 			.setPopup(popup);
+// 	});
+
+
+// $.get("http://api.openweathermap.org/data/2.5/forecast", {
+// 	APPID: weatherKey,
+// 	q: "New Braunfels, US",
+// 	units: "imperial"
+// })
+//
+// 	.done(function (data) {
+//
+// 	});
+
 //weather.main; .description
 //main. temp_max; temp_min; feels_like; humidity
 // wind.gust

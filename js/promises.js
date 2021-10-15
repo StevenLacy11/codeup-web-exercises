4aaac43c41f7456f2fad089fe62648e6bcd9c63b
Steven Lacy <steven.lacy11@gmail.com>
2021-10-15 13:54:01 -0500

    promises exercise complete

---
+++ js/promises.js
@@ -0,0 +1,14 @@
+"use strict"
+$(document).ready(function () {
+
+	fetch(`https://api.github.com/users/StevenLacy11/events`, {headers: {"Authorization": promiseRing}})
+		.then((response) => response.json())
+		.then((Data) => {
+			console.log(Data[0].created_at);
+		})
+	const chill = ms => new Promise(function (resolve) {
+		setTimeout(resolve, ms);
+	});
+	chill(6000).then(() => console.log(`runs after 6 seconds`));
+
+})
